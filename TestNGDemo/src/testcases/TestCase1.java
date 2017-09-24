77e74aa43717fcdc7f69c2772fcc69fa54ed9c0d
Kunal <Kunal@Kunal-PC>
2017-09-24 18:23:36 +0530

    Initial checkin

---
+++ TestNGDemo/src/testcases/TestCase1.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void doTest() {
+		System.out.println("I am running fine..");
+		System.out.println("My first checkin");
+	}
+
+}
